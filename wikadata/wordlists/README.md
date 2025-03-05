1326895373482735a857c57b1f66b7bd2d57e621
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-03-05 21:31:16 +0800

    Create wordlist parser

---
+++ wikadata/wordlists/README.md
@@ -0,0 +1,14 @@
+# Word lists
+
+Generated word lists from the parsed dictionaries.
+
+## Data Format
+
+### TXT Word List
+
+```txt
+word
+another
+new
+...
+```
---
+++ wikadata/wordlists/parser.py
@@ -0,0 +1,61 @@
+import json
+from pathlib import Path
+from utils.logger import logger
+from utils.graceful_exit import on_exit
+
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+
+def main():
+    dictionaries_dir = SCRIPT_DIR.parent / "dictionaries"
+    wordlists: dict[str, set[str]] = {}
+
+    on_exit(
+        lambda: export_wordlists(wordlists),
+        message="Process interrupted. Saving word lists...",
+    )
+
+    generate_wordlists(dictionaries_dir, wordlists)
+    export_wordlists(wordlists)
+
+
+def generate_wordlists(dictionaries_dir: Path, wordlists: dict[str, set[str]]) -> bool:
+    """Generates word lists from parsed dictionaries."""
+    for file_path in dictionaries_dir.glob("*/parsed/*.json"):
+        logger.info(f"Processing file: {file_path}")
+
+        with file_path.open("r", encoding="utf-8") as file:
+            data = json.load(file)
+            lang = data["meta"]["lang"]
+
+            wordlists.setdefault(lang, set())
+
+            for entry in data["entries"]:
+                wordlists[lang].add(entry["word"])
+
+    logger.info(f"Generated {len(wordlists)} word lists.")
+    return True
+
+
+def export_wordlists(wordlists: dict[str, set[str]]) -> bool:
+    """Exports word lists to JSON files."""
+    if not wordlists:
+        logger.warning("No word lists to export.")
+        return False
+
+    output_dir = SCRIPT_DIR / "processed_data"
+    output_dir.mkdir(exist_ok=True)
+
+    for lang, words in wordlists.items():
+        output_path = output_dir / f"wordlist_{lang}.txt"
+        with output_path.open("w", encoding="utf-8") as file:
+            sorted_words = sorted(words)
+            file.writelines("\n".join(sorted_words))
+
+    logger.info(f"Word lists successfully exported to {output_dir}.")
+    return True
+
+
+if __name__ == "__main__":
+    main()
